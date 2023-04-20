245620c49fb3a760753760d22cb4ef46cd76002b
Sophie White <sophie.white@mordrid.net>
2023-04-20 23:32:38 +0200

    get rid of duplicate code

--- src/pages/PetPages/CreatePet.jsx
+++ src/pages/PetPages/CreatePet.jsx
@@ -1,171 +1,171 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Spinner from "../../components/Spinner";
-import { BsCheckCircle } from "react-icons/bs";
-import Button from "react-bootstrap/Button";
+// import { useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// import Spinner from "../../components/Spinner";
+// import { BsCheckCircle } from "react-icons/bs";
+// import Button from "react-bootstrap/Button";
 
-function CreatePet() {
-    const storedToken = localStorage.getItem('authToken');
-    const navigate = useNavigate();
-    const [uploading, setUploading] = useState(false);
+// function CreatePet() {
+//     const storedToken = localStorage.getItem('authToken');
+//     const navigate = useNavigate();
+//     const [uploading, setUploading] = useState(false);
 
-    const [newImage, setNewImage] = useState("");
-    const [ newPet, setNewPet ] = useState({
-        name: '',
-        age: 0,
-        species: '',
-        breed: '',
-        personality: '',
-        imageUrl: ''
-    })
+//     const [newImage, setNewImage] = useState("");
+//     const [ newPet, setNewPet ] = useState({
+//         name: '',
+//         age: 0,
+//         species: '',
+//         breed: '',
+//         personality: '',
+//         imageUrl: ''
+//     })
 
-    const changeHandler = (target) => {
-        setNewPet((prevState) => {
-            return {...prevState, [target.name] : target.value}
-        })
-    }
+//     const changeHandler = (target) => {
+//         setNewPet((prevState) => {
+//             return {...prevState, [target.name] : target.value}
+//         })
+//     }
 
-    // image file upload
-    const handleFileUpload = (e) => {
-      e.preventDefault();
+//     // image file upload
+//     const handleFileUpload = (e) => {
+//       e.preventDefault();
     
-      const uploadData = new FormData();
-      uploadData.append("imageUrl", e.target.files[0]);
-      setUploading(true);
+//       const uploadData = new FormData();
+//       uploadData.append("imageUrl", e.target.files[0]);
+//       setUploading(true);
   
-      axios
-        .post(`${process.env.REACT_APP_SERVER_URL}/api/upload`, uploadData, {
-          headers: { Authorization: `Bearer ${storedToken}` },
-        })
-        .then((response) => {
-          const imageFile = response.data.fileUrl;
-          setNewImage(imageFile);
-          setNewPet((prevState) => ({
-            ...prevState, imageUrl: imageFile
-          }));
-        })
-        .catch((err) => console.log("Error while uploading the file: ", err))
-        .finally(() => setUploading(false));
-    };
+//       axios
+//         .post(`${process.env.REACT_APP_SERVER_URL}/api/upload`, uploadData, {
+//           headers: { Authorization: `Bearer ${storedToken}` },
+//         })
+//         .then((response) => {
+//           const imageFile = response.data.fileUrl;
+//           setNewImage(imageFile);
+//           setNewPet((prevState) => ({
+//             ...prevState, imageUrl: imageFile
+//           }));
+//         })
+//         .catch((err) => console.log("Error while uploading the file: ", err))
+//         .finally(() => setUploading(false));
+//     };
 
-    const submitForm = (event) => {
-        event.preventDefault();
+//     const submitForm = (event) => {
+//         event.preventDefault();
 
-        axios
-          .post(`${process.env.REACT_APP_SERVER_URL}/api/pets/`, newPet, { headers: {Authorization: `Bearer ${storedToken}`}})
-          .then((response) => {
-            navigate("/pets");
-          })
-          .catch((err) => console.error(err));
-      }
+//         axios
+//           .post(`${process.env.REACT_APP_SERVER_URL}/api/pets/`, newPet, { headers: {Authorization: `Bearer ${storedToken}`}})
+//           .then((response) => {
+//             navigate("/pets");
+//           })
+//           .catch((err) => console.error(err));
+//       }
     
       
-      return (
-        <section>
-          <h1>Create Pet</h1>
-          <form
-            onSubmit={(event) => {
-              submitForm(event, newImage);
-            }}
-          >
-            <label>
-              {" "}
-              Name:
-              <input
-                type="text"
-                name="name"
-                value={newPet.name}
-                onChange={(event) => {
-                  changeHandler(event.target);
-                }}
-              />
-            </label>
+//       return (
+//         <section>
+//           <h1>Create Pet</h1>
+//           <form
+//             onSubmit={(event) => {
+//               submitForm(event, newImage);
+//             }}
+//           >
+//             <label>
+//               {" "}
+//               Name:
+//               <input
+//                 type="text"
+//                 name="name"
+//                 value={newPet.name}
+//                 onChange={(event) => {
+//                   changeHandler(event.target);
+//                 }}
+//               />
+//             </label>
 
-            <label>
-              {" "}
-              Age:
-              <input
-                type="number"
-                name="age"
-                value={newPet.age}
-                onChange={(event) => {
-                  changeHandler(event.target);
-                }}
-              />
-            </label>
+//             <label>
+//               {" "}
+//               Age:
+//               <input
+//                 type="number"
+//                 name="age"
+//                 value={newPet.age}
+//                 onChange={(event) => {
+//                   changeHandler(event.target);
+//                 }}
+//               />
+//             </label>
 
-            <label>
-              Species:
-              <select
-                name="species"
-                value={newPet.species}
-                onChange={(event) => {
-                  changeHandler(event.target);
-                }}
-              >
-                <option value="">select...</option>
-                {["cat", "dog"].map((species) => (
-                  <option key={species} value={species}>
-                    {species}
-                  </option>
-                ))}
-              </select>
-            </label>
+//             <label>
+//               Species:
+//               <select
+//                 name="species"
+//                 value={newPet.species}
+//                 onChange={(event) => {
+//                   changeHandler(event.target);
+//                 }}
+//               >
+//                 <option value="">select...</option>
+//                 {["cat", "dog"].map((species) => (
+//                   <option key={species} value={species}>
+//                     {species}
+//                   </option>
+//                 ))}
+//               </select>
+//             </label>
 
-            <label>
-              {" "}
-              Breed:
-              <input
-                type="text"
-                name="breed"
-                value={newPet.breed}
-                onChange={(event) => {
-                  changeHandler(event.target);
-                }}
-              />
-            </label>
+//             <label>
+//               {" "}
+//               Breed:
+//               <input
+//                 type="text"
+//                 name="breed"
+//                 value={newPet.breed}
+//                 onChange={(event) => {
+//                   changeHandler(event.target);
+//                 }}
+//               />
+//             </label>
 
-            <label>
-              {" "}
-              Personality:
-              <select
-                name="personality"
-                value={newPet.personality}
-                onChange={(event) => {
-                  changeHandler(event.target);
-                }}
-              >
-                <option value="">select...</option>
-                {[
-                  "Introvert",
-                  "Outgoing",
-                  "Playful",
-                  "Protective",
-                  "Independant",
-                  "Affectionate",
-                ].map((personality) => (
-                  <option key={personality} value={personality}>
-                    {personality}
-                  </option>
-                ))}
-              </select>
-            </label>
+//             <label>
+//               {" "}
+//               Personality:
+//               <select
+//                 name="personality"
+//                 value={newPet.personality}
+//                 onChange={(event) => {
+//                   changeHandler(event.target);
+//                 }}
+//               >
+//                 <option value="">select...</option>
+//                 {[
+//                   "Introvert",
+//                   "Outgoing",
+//                   "Playful",
+//                   "Protective",
+//                   "Independant",
+//                   "Affectionate",
+//                 ].map((personality) => (
+//                   <option key={personality} value={personality}>
+//                     {personality}
+//                   </option>
+//                 ))}
+//               </select>
+//             </label>
 
-            <input type="file" onChange={(e) => handleFileUpload(e)} />
-            {uploading && (
-              <p>
-                Image uploading <Spinner />
-              </p>
-            )}
-            {newImage && <BsCheckCircle color="green" />}
+//             <input type="file" onChange={(e) => handleFileUpload(e)} />
+//             {uploading && (
+//               <p>
+//                 Image uploading <Spinner />
+//               </p>
+//             )}
+//             {newImage && <BsCheckCircle color="green" />}
 
-            <Button type="submit">
-              Create
-            </Button>
-          </form>
-        </section>
-      );  
-}
+//             <Button type="submit">
+//               Create
+//             </Button>
+//           </form>
+//         </section>
+//       );  
+// }
 
-export default CreatePet;
+// export default CreatePet;
