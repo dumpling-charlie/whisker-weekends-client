0fb570f5794ec2ff1cf7f0066b804b619b66cdd4
Sophie White <sophie.white@mordrid.net>
2023-04-21 09:21:26 +0200

    footer styling final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,32 +9,31 @@ function Footer() {
         <ul className="d-flex justify-content-center align-items-center list-unstyled flex-row ">
           <li className="links d-flex align-items-center">
             <p>Mattie Barton</p>
-            <a
-              href="https://github.com/maitlandbarton"
-            >
-                <AiFillGithub size="2rem" />
+            <a href="https://github.com/maitlandbarton" target="_blank">
+              <AiFillGithub size="2rem" />
             </a>
           </li>
           <li className="links d-flex align-items-center">
             <a
               href="https://www.linkedin.com/in/mattiebarton/"
+              target="_blank"
+              className="footer-spacing-r"
             >
-                <AiFillLinkedin size="2rem" />
+              <AiFillLinkedin size="2rem" />
             </a>
           </li>
           <li className="links d-flex align-items-center">
-            <p>Sophie White</p>
-            <a
-              href="https://github.com/SElizaWhi"
-            >
-                <AiFillGithub size="2rem" />
+            <p className="footer-spacing-l">Sophie White</p>
+            <a href="https://github.com/SElizaWhi" target="_blank">
+              <AiFillGithub size="2rem" />
             </a>
           </li>
           <li className="links d-flex align-items-center">
             <a
               href="https://www.linkedin.com/in/sophie-e-white/"
+              target="_blank"
             >
-                <AiFillLinkedin size="2rem" />
+              <AiFillLinkedin size="2rem" />
             </a>
           </li>
         </ul>
